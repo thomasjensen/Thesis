cd252404c3fb382b631e55a9a0f01089ac0d561f
Thomas Jensen <thomas.jensen@eup.gess.ethz.ch>
2011-07-15 14:26:48 +0200

    Third

---
+++ VoteChoice_Chapter/commit.sh
@@ -0,0 +1,8 @@
+#! /bin/sh
+
+cd /Users/thomasjensen/Documents/Thesis/
+
+git add .
+git commit -a -s
+git remote add origin git@github.com:thomasjensen/Thesis
+git push origin master
---
+++ commit.sh
@@ -0,0 +1,7 @@
+#! /bin/sh
+
+cd /Users/thomasjensen/Documents/Thesis/
+DATE=`date`
+git add .
+git commit -m `$DATE`
+git push origin master
